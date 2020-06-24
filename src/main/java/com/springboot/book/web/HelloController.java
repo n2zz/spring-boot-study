177e0ff9042277876d5d6290a9fbec30837ee724
byeongjoon_kim <byeongjoon_kim@kolon.com>
2020-06-24 16:38:13 +0900

    autotest

---
+++ src/main/java/com/springboot/book/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.springboot.book.web;
+
+import com.springboot.book.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello SpringBoot";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+    return new HelloResponseDto(name, amount);
+    }
+}
